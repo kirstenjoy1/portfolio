774a9841c608a4ce8475854d0347a7ec131171a9
kirstenjoy1 <75848987+kirstenjoy1@users.noreply.github.com>
2021-06-01 17:34:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ let itemOne = document.getElementById('about-me-container');
 function increaseHeight(){
     itemOne.style.height = '500px';
   }
-  itemOne.addEventListener('keydown', increaseHeight());
+  itemOne.addEventListener('click', increaseHeight());
